2ea117d5066e6760436f044602eb7440401c0883
simpleman383 <simpleman383@gmail.com>
2017-08-07 17:49:48 +0300

    Log AutoUpdate
    Added new method for log auto updating via new thread. Thread reads log
    file every N seconds and updates the text view.

--- app/src/main/java/com/simpleman383/testapp/LogActivity.java
+++ app/src/main/java/com/simpleman383/testapp/LogActivity.java
@@ -2,9 +2,11 @@ package com.simpleman383.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.util.logging.Logger;
@@ -17,6 +19,8 @@ public class LogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_log);
 
         initWidgets();
+        Thread autoUpdate = autoUpdateInit();
+        autoUpdate.start();
     }
 
     private void initWidgets(){
@@ -35,5 +39,36 @@ public class LogActivity extends AppCompatActivity {
         TextView logWindow = (TextView)findViewById(R.id.log_window);
         logWindow.setText("");
         logWindow.setText(data);
+        ScrollView scrollView = (ScrollView)findViewById(R.id.scrollView);
+        scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+    }
+
+    private Thread autoUpdateInit(){
+        final int SLEEP_INTERVAL = 400;
+        Thread autoUpdate = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(SLEEP_INTERVAL);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                String data = SMSLogger.createLogger().readAll(getApplicationContext());
+                                TextView logWindow = (TextView)findViewById(R.id.log_window);
+                                logWindow.setText("");
+                                logWindow.setText(data);
+
+                                ScrollView scrollView = (ScrollView)findViewById(R.id.scrollView);
+                                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                            }
+                        });
+                    }
+                } catch (InterruptedException ex) {
+                    Log.e("LogActivity", "AutoUpdateThread : " + ex);
+                }
+            }
+        };
+        return autoUpdate;
     }
 }
